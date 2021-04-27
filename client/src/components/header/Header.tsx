3648ead40cb0717c3d5852b5df5279f56be1a89d
Maksim1108 <maksim.lyanko@bk.ru>
2021-04-27 19:56:52 +0600

    create Header

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Logo from '../../assets/header/logo.svg'
+
+const Header: React.FC = () => {
+
+  const links: string[] = ['Home', 'Movies', 'Live', 'Series', 'Kids',]
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__content row">
+          <img src={Logo} alt="" className="header__logo" />
+          <label>
+            <input className="header__search" type="text" placeholder="Search titles here..." />
+          </label>
+          <nav className="header__nav">
+            <ul className="header__list flex">
+              {links.map(link => {
+                return <li key={link}><a className="header__link" href="/">{link}</a></li>
+              })}
+            </ul>
+          </nav>
+          <div className="header__buttons">
+            <button className="header__btn transparent">Sign in</button>
+            <button className="header__btn">Register</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
