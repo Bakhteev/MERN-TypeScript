2580414929d5b23109fce357ee2c6eba8b16ecd1
Bogdan-create <bogdan-shigaev@mail.ru>
2021-05-06 22:20:04 +0600

    added query parameters

--- server/src/user/schema/rewiew.schema.ts
+++ server/src/user/schema/rewiew.schema.ts
@@ -2,6 +2,8 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import * as mongoose from 'mongoose'
 import { User } from './user.schema'
 import { Document } from 'mongoose'
+import { Film } from 'src/films/schema/film.schema'
+import { Serial } from 'src/serial/schema/serial.schema'
 
 export type ReviewDocument = Review & Document
 
@@ -12,6 +14,14 @@ export class Review {
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
   user_id: User
+
+  @Prop({
+    type: [
+      { type: mongoose.Schema.Types.ObjectId, ref: 'Film' },
+      { type: mongoose.Schema.Types.ObjectId, ref: 'Serial' },
+    ],
+  })
+  film_id: Film | Serial
 }
 
 export const ReviewSchema = SchemaFactory.createForClass(Review)
