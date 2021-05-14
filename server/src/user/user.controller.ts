4224c63f0cbfc4c0cd2d0aa622277e2a2feb0218
Bogdan-create <bogdan-shigaev@mail.ru>
2021-05-14 15:37:04 +0600

    added user hashing password

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UserService } from './user.service';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UserService } from './user.service'
+import { Response } from 'express'
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
 
   @Post('/registration')
-  registration(@Body() user: CreateUserDto){
-    return this.userService.registration(user)
+  registration(@Body() user: CreateUserDto, @Res() res: Response) {
+    return this.userService
+      .registration(user)
+      // .then((token) => res.cookie('token', token))
   }
 }
