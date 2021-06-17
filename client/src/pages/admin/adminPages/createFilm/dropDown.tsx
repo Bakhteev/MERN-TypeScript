76ed6ee6b94c3c4502a1403fb2851a2318b6d351
Bogdan-create <bogdan-shigaev@mail.ru>
2021-06-17 15:01:56 +0600

    started dropdown

--- client/src/pages/admin/adminPages/createFilm/dropDown.tsx
+++ client/src/pages/admin/adminPages/createFilm/dropDown.tsx
@@ -4,6 +4,7 @@ interface DropDownProps {
   items: DropDownItems[]
   keyWord: string
   chooseItem: Function
+  state: DropDownItems[] | []
   setState: Function
 }
 
@@ -16,20 +17,51 @@ const DropDown: React.FC<DropDownProps> = ({
   items,
   keyWord,
   chooseItem,
+  state,
   setState,
 }) => {
-  const [active, setActive] = useState<any>({ name: '', _id: '' })
+  const [choosedItem, setChoosedItem] = useState<any>({ name: '', _id: '' })
+  const [choosedItems, setChoosedItems] = useState<any[]>([])
+  const [active, setActive] = useState<boolean>(false)
 
   const handleActive = (item: any) => {
-    setActive(item)
+    // setActive(active ? false : true)
+    setChoosedItems([...choosedItems, item._id])
+    setChoosedItem(item)
     chooseItem(item._id, setState)
   }
 
+  const aaaa = (item: DropDownItems) => {
+    // const activeClassName1 = state.filter((choosedItem) => item === choosedItem)
+    const activeClassName = state.find(() => item)
+    if (activeClassName) {
+      return true
+    }
+    return false
+  }
+
+  // const activeClassName = state.filter()
+
   return (
-    <div style={{ overflow: 'hidden' }}>
-      <span>{active.name || `Выберите ${keyWord}`}</span>
-      {items.map((item) => (
-        <div key={item._id} onClick={() => handleActive(item)}>
+    <div
+      className={`dropdown ${active ? 'active' : ''}`}
+      onClick={() => {
+        setActive(active ? false : true)
+      }}
+    >
+      <span className="dropdown__choosed flex">
+        {choosedItem.name || `Выберите ${keyWord}`}
+      </span>
+      {items.map((item: DropDownItems) => (
+        <div
+          className={`dropdown__item ${
+            choosedItems.filter((id) => id === item._id)[0] === item._id
+              ? 'active'
+              : ''
+          }`}
+          key={item._id}
+          onClick={() => handleActive(item)}
+        >
           {item.name}
         </div>
       ))}
